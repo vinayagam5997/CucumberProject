ef4831890b3caff5a596e15c49c02bf77e564dfd
n520492 <vinayagam5997@gmail.com>
2023-08-17 07:30:25 +0100

    Fisrt commit in 1.1-copy

---
+++ src/test/java/org/vinay/SampleClass.java
@@ -0,0 +1,11 @@
+package org.vinay;
+
+/**
+ * @author Vinayagamoorthy Ezhumalai <n520492> on 17/08/2023
+ */
+
+public class SampleClass {
+    public static void main(String[] args) {
+        System.out.println("vinay");
+    }
+}
